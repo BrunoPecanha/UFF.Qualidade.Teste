443aa541bba6c4a9c6e120809e211203e6d98f03
Bruno Cipriani Cerqueira <71668982+brunocipriani@users.noreply.github.com>
2022-12-17 11:24:26 -0300

    Adiciona 11 contas ao usuário Hulk

--- app/seed_sql_financeiro_qt.sql
+++ app/seed_sql_financeiro_qt.sql
@@ -1,46 +1,79 @@
 -- ADMINISTRADORES
 
 insert into administradores(id, nome, cpf, senha) values
-(null, 'Thanos', '13676616766', '1234')
+(null, 'Thanos', '13676616766', '1234');
 
 insert into administradores(id, nome, cpf, senha) values
-(null, 'Kang', '11111111111', '1234')
+(null, 'Kang', '11111111111', '1234');
 
 -- USUARIOS
 
 insert into usuarios(id, nome, cpf, senha) values
-(null, 'Homem de Ferro', '12345678910', '1234')
+(null, 'Homem de Ferro', '12345678910', '1234');
 
 insert into usuarios(id, nome, cpf, senha) values
-(null, 'Thor', '00011122233', '1234')
+(null, 'Thor', '00011122233', '1234');
 
 insert into usuarios(id, nome, cpf, senha) values
-(null, 'Hulk', '09876543210', '1234')
+(null, 'Hulk', '09876543210', '1234');
 
 -- CATEGORIAS
 
 insert into categorias(id, descricao) values
-(null, 'Compras')
+(null, 'Compras');
 
 insert into categorias(id, descricao) values
-(null, 'Moradia')
+(null, 'Moradia');
 
 insert into categorias(id, descricao) values
-(null, 'Transporte')
+(null, 'Transporte');
 
 insert into categorias(id, descricao) values
-(null, 'Lazer')
+(null, 'Lazer');
 
 insert into categorias(id, descricao) values
-(null, 'Pagamento')
+(null, 'Pagamento');
 
 -- CONTAS
 
 insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
-(null, (select id from usuarios where cpf = '12345678910'), 'Bradesco', '011', '1276', '111222')
+(null, (select id from usuarios where cpf = '09876543210'), 'Bradesco', '011', '1276', '111222');
 
 insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
-(null, (select id from usuarios where cpf = '00011122233'), 'Itaú', '562', '6214', '668774')
+(null, (select id from usuarios where cpf = '09876543210'), 'Itaú', '562', '6214', '668774');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 1', '562', '0001', '000001');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 2', '562', '0002', '000002');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 3', '562', '0003', '000003');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 4', '562', '0004', '000004');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 5', '562', '0005', '000005');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 6', '562', '0006', '000006');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 7', '562', '0007', '000007');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 8', '562', '0008', '000008');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 9', '562', '0009', '000009');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 10', '562', '0010', '000010');
+
+insert into contas (id, id_usuario, nome_conta, banco, agencia, conta_corrente) values
+(null, (select id from usuarios where cpf = '09876543210'), 'Teste 11', '562', '0011', '000011');
 
 -- LANÇAMENTOS
 
@@ -52,7 +85,7 @@ insert into lancamentos (id, id_conta, id_categoria, valor, operacao, data, desc
 150.00,
 'D',
 STR_TO_DATE('17-10-2022', '%d-%m-%Y'),
-'Compras para a semana')
+'Compras para a semana');
 
 insert into lancamentos (id, id_conta, id_categoria, valor, operacao, data, descricao) values
 (null, 
@@ -62,7 +95,7 @@ insert into lancamentos (id, id_conta, id_categoria, valor, operacao, data, desc
 10.000,
 'C',
 STR_TO_DATE('15-10-2022', '%d-%m-%Y'),
-'Pagamento do salário')
+'Pagamento do salário');
 
 
 insert into lancamentos (id, id_conta, id_categoria, valor, operacao, data, descricao) values
@@ -73,7 +106,7 @@ insert into lancamentos (id, id_conta, id_categoria, valor, operacao, data, desc
 50,
 'D',
 STR_TO_DATE('18-10-2022', '%d-%m-%Y'),
-'Ida para faculdade')
+'Ida para faculdade');
 
 
 
