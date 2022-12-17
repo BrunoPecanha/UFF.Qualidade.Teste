527b03fe7ba32804f8b15b4ed0c97f61a9257e0d
brunocipriani <bcipriani@outlook.com>
2022-12-17 12:51:29 -0200

    Testes Cliente/Conta Corrente v2

--- automatizacao/test/test/Cliente.java
+++ automatizacao/test/test/Cliente.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 import static org.junit.Assert.*;
-
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
